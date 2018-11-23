63e2b59760fd08100a513abd2eb48cb4b2de4dd4
Pascal <pascal@luther.ch>
2018-11-23 13:47:38 +0100

    added "allowSyntheticDefaultImports": true

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,9 +2,9 @@
 /** Takes ref and queries it ********************/
 /** Returning the JS document as a Javascript Obj */
 /**************************************************/
-export declare const queryFirestore: (query: any) => Promise<any>
+export declare const queryFirestore: (query: any) => Promise<any>;
 /***************************************************/
 /** Takes a Snapshot and returns the queried item */
 /** adding _id and _path to the queried document  */
 /**************************************************/
-export declare const unwrapFirestoreDoc: (snapshot: any) => any
+export declare const unwrapFirestoreDoc: (snapshot: any) => any;
