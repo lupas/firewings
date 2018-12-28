e890e09b4bab7a45f8545700c51a1dc9152f7939
Command2017 <daniel.steinegger@students.fhnw.ch>
2018-12-28 18:12:34 +0100

    Added Documentation and did same makeup

--- src/index.js
+++ src/index.js
@@ -3,36 +3,33 @@
 /** Removes the properties id and path from the copy of a object  and set() it to firebase  */
 /**************************************************/
 
-export const setToFirestore = async function(query, payload) {
-  let clone = Object.assign({}, payload);
-  if (clone.id) delete clone.id;
-  if (clone.path) delete clone.path;
+export const setToFirestore = async function(ref, payload) {
+  let clone = Object.assign({}, payload)
+  if (clone.id) delete clone.id
+  if (clone.path) delete clone.path
 
   try {
-    const ref = await query.set(clone);
+    await ref.set(clone)
   } catch (e) {
-    return Promise.reject(e);
+    return Promise.reject(e)
   }
-};
+}
 
 /***************************************************/
 /** Takes a query and a payload ********************/
 /** Returns the payload with the properties id and path */
 /**************************************************/
 
-export const addToFirestore = async function(query, payload) {
-  console.log("HIEEER", query, payload);
+export const addToFirestore = async function(ref, payload) {
   try {
-    const ref = await query.add(payload);
-    console.log("1");
-    payload.id = ref.id;
-    payload.path = ref.path;
-    console.log("2");
-    return payload;
+    const snapshot = await ref.add(payload)
+    payload.id = snapshot.id
+    payload.path = snapshot.path
+    return payload
   } catch (e) {
-    return Promise.reject(e);
+    return Promise.reject(e)
   }
-};
+}
 
 /***************************************************/
 /** Takes ref and queries it ********************/
@@ -41,12 +38,12 @@ export const addToFirestore = async function(query, payload) {
 
 export const queryFirestore = async function(query) {
   try {
-    const snapshot = await query.get();
-    return unwrapFirestoreDoc(snapshot);
+    const snapshot = await query.get()
+    return unwrapFirestoreDoc(snapshot)
   } catch (e) {
-    return Promise.reject(e);
+    return Promise.reject(e)
   }
-};
+}
 
 /***************************************************/
 /** Takes a Snapshot and returns the queried item */
@@ -56,27 +53,27 @@ export const unwrapFirestoreDoc = function(snapshot) {
   //If it is a multi-document query
   // returns am array of items
   if (snapshot.docs) {
-    let items = [];
+    let items = []
     for (const doc of snapshot.docs) {
-      let item = doc.data();
-      item.id = doc.id;
-      item.path = doc.ref.path;
-      items.push(item);
+      let item = doc.data()
+      item.id = doc.id
+      item.path = doc.ref.path
+      items.push(item)
     }
     if (!items) {
-      return [];
+      return []
     }
-    return items;
+    return items
   }
 
   //If it is a single-document query
   // returns a single item
   if (!snapshot.docs) {
-    let item = snapshot.data();
+    let item = snapshot.data()
     if (item) {
-      item.id = snapshot.id;
-      item.path = snapshot.ref.path;
+      item.id = snapshot.id
+      item.path = snapshot.ref.path
     }
-    return item;
+    return item
   }
-};
+}
